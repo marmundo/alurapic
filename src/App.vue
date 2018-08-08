aba7c807966ab9120283c7bed6db3b8e46680e00
Marcelo Damasceno <marcelodamasceno@gmail.com>
2018-08-08 13:14:00 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <h1>{{ titulo}}</h1>
+  <img class="img-responsive" :src="foto.url" :alt="foto.titulo" />
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      titulo:"Alura Pic",
+      foto:{
+        url:"https://abrilsuperinteressante.files.wordpress.com/2016/03/cachorro.png",
+        titulo:"cachorro"
+      }
+    }
+  }
+ 
+}
+</script>
+
+<style>
+  img{
+    width: 10em
+  }
+</style>
