7ed017ab73fda2c938e49c5c2ea9799c3f26a16d
Marcelo Damasceno <marcelodamasceno@gmail.com>
2018-08-08 13:15:34 -0300

    Adiciona componente de painel

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,71 @@
+
+
 <template>
-  <div>
-  <h1>{{ titulo}}</h1>
-  <img class="img-responsive" :src="foto.url" :alt="foto.titulo" />
+
+
+
+  <div class="corpo">
+  <h1 class="centralizado">{{ titulo}}</h1>
+  
+  <ul class="lista-fotos">
+    
+    <li class="lista-fotos-item" v-for="foto of fotos">
+      <meu-painel :titulo="foto.titulo">
+         <img slot="painel-conteudo" class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+      </meu-painel>    
+    </li>
+    
+  </ul>
   </div>
 </template>
 
 <script>
+
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
+
+  components:{
+    'meu-painel':Painel
+  },
+
   data(){
     return{
       titulo:"Alura Pic",
-      foto:{
-        url:"https://abrilsuperinteressante.files.wordpress.com/2016/03/cachorro.png",
-        titulo:"cachorro"
-      }
+      fotos:[]   
     }
+  },
+  created(){
+     this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err=>console.log(err));  
+    
   }
  
 }
 </script>
 
 <style>
-  img{
-    width: 10em
+   .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+ 
+  .imagem-responsiva{
+    width: 100%
   }
 </style>
